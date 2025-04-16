953ad23b9d442575b986b693674457841d640ab6
Ahmet ÖZTÜRK <ahmet.ztrk1990@gmail.com>
2025-04-16 23:22:21 +0300

    update

--- TravelTripProje/Controllers/iletisimController.cs
+++ TravelTripProje/Controllers/iletisimController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TravelTripProje.Models.Sınıflar;
 
 namespace TravelTripProje.Controllers
 {
     public class iletisimController : Controller
     {
         // GET: iletisim
+        Context c = new Context();
         public ActionResult Index()
         {
-            return View();
+            var degerler = c.iletisims.ToList();
+            return View(degerler);
         }
     }
 }
